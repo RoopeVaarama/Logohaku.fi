d8795bf14a2d59a0ab2ec0df6e9bf76e5b2254e4
Topias Peiponen <topiaspe@metropolia.fi>
2021-02-17 13:05:42 +0200

    Duck working 3d model

--- src/Components/SceneComponent.js
+++ src/Components/SceneComponent.js
@@ -1,7 +1,6 @@
 import {Engine, Model, Scene} from 'react-babylonjs';
 import {Vector3} from '@babylonjs/core';
 import React, { Suspense, useEffect, useRef } from 'react';
-import '@babylonjs/loaders/glTF';
 
 /**
  * React component for utilizing Babylon.js
@@ -11,18 +10,18 @@ import '@babylonjs/loaders/glTF';
  * 
  */
 
- const baseUrl = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/"
+const baseUrl = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/"
 const SceneComponent = (props) => {
     return (
             <Engine antialias adaptToDeviceRatio canvasId="asdf">
                 <Scene>
                     <arcRotateCamera name="camera1" target={Vector3.Zero()} alpha={Math.PI / 2} beta={Math.PI / 4} radius={8} />
                     <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()} />
-                    <Suspense fallback={<box></box>}>
+                    <Suspense fallback={<box position={new Vector3(0,0,0)}></box>}>
                         <Model 
                         position={new Vector3(0,0,0)}
-                        rootUrl={''}
-                        sceneFilename={''}
+                        rootUrl={'./Models/'}
+                        sceneFilename={'Duck.gltf'}
                         pluginExtension='.gltf'
                         ></Model>
                     </Suspense>
