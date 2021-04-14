427e3536d8f4181c1c89acee88aae21404637b2f
Topias Peiponen <topiaspe@metropolia.fi>
2021-04-14 13:39:05 +0300

    CSS modified in results page, added info cards

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -13,6 +13,10 @@ import Box from '@material-ui/core/Box';
 import EditIcon from "@material-ui/icons/Edit";
 import IconButton from "@material-ui/core/IconButton";
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import SceneComponent from "../SceneComponent";
 import ProductsList from "../ProductsList/ProductsList";
 import PresetsList from "../PresetsList/PresetsList";
@@ -73,6 +77,16 @@ const useStyles = makeStyles({
     position: 'absolute',
     right: 0,
     top: 0
+  },
+  infoCard: {
+    flexBasis: '15%',
+    maxWidth: '20%',
+    marginBottom: '8px',
+    marginRight: '32px',
+    padding: '16px'
+  },
+  pickerTable: {
+    flexBasis: '80%'
   }
 })
 
@@ -361,28 +375,51 @@ const Results = ({ lang, handleAddToCart }) => {
   return (
     <div>
       <h1>{TextValues.results(lang)}</h1>
-      <Table striped bordered className="LogoPicker">
-        <tbody>
-          <tr>
-            {mapLogos()}
-            <EditLogoDialog
-              selectedValue={selectedEditableLogo}
-              open={logoPickerDialogOpen}
-              onClose={handleCloseLogos}
-              ytunnus={response.code}
-            />
-            <td>
-              <Button className="LogoPickerButton" onClick={() => setNewLogoPickerDialogOpen(true)}>+</Button>
-              <NewLogoDialog
-              selectedValue={response.files}
-              open={newLogoPickerDialogOpen}
-              onClose={handleCloseNewLogo}
-              ytunnus={response.code}
-            />
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="PickerContainer">
+        <Card className={styles.infoCard}>
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+              {TextValues.logoPickerInfo(lang).title}
+            </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              {TextValues.logoPickerInfo(lang).info}
+            </Typography>
+          </CardContent>
+        </Card>
+        <Table striped bordered className="LogoPicker">
+          <tbody>
+            <tr>
+              {mapLogos()}
+              <EditLogoDialog
+                selectedValue={selectedEditableLogo}
+                open={logoPickerDialogOpen}
+                onClose={handleCloseLogos}
+                ytunnus={response.code}
+              />
+              <td>
+                <Button className="LogoPickerButton" onClick={() => setNewLogoPickerDialogOpen(true)}>+</Button>
+                <NewLogoDialog
+                selectedValue={response.files}
+                open={newLogoPickerDialogOpen}
+                onClose={handleCloseNewLogo}
+                ytunnus={response.code}
+              />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <div className="PickerContainer">
+        <Card className={styles.infoCard}>
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+              {TextValues.colorPickerInfo(lang).title}
+            </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              {TextValues.colorPickerInfo(lang).info}
+            </Typography>
+          </CardContent>
+        </Card>
       <Table striped bordered className="ColorPicker">
         <tbody>
           <tr>
@@ -406,6 +443,7 @@ const Results = ({ lang, handleAddToCart }) => {
           </tr>
         </tbody>
       </Table>
+      </div>
       <div className="PreviewerWindow">
         <div className="ButtonPanel">
           <Button className="CatalogButton" onClick={toggleProductsList}>
@@ -419,20 +457,7 @@ const Results = ({ lang, handleAddToCart }) => {
             >
               <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V2zm8.5 0v8H15V2H8.5zm0 9v3H15v-3H8.5zm-1-9H1v3h6.5V2zM1 14h6.5V6H1v8z" />
             </svg>
-            <div className="CatalogText">Catalog</div>
-          </Button>
-          <Button className="PresetsButton" onClick={togglePresetsList}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="50%"
-              height="50%"
-              fill="currentColor"
-              className="PresetsIcon"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zm2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5z" />
-            </svg>
-            <div className="PresetsText">Presets</div>
+            <div className="CatalogText">{TextValues.catalog(lang)}</div>
           </Button>
         </div>
         <div className="Products" hidden={productsList}>
@@ -440,9 +465,6 @@ const Results = ({ lang, handleAddToCart }) => {
             productsObjects={productsObjects}
             selectProduct={selectProduct}
           ></ProductsList>
-        </div>
-        <div className="Presets" hidden={presetsList}>
-          <PresetsList></PresetsList>
         </div>
         <SceneComponent
           lang={lang}
